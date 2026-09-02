93f595aac3e9450d71ca4baba08b02686d60c15f
Axel Talmet <a@bjc.gg>
2026-09-03 00:05:16 +0100

    checkin 0.2.0

--- Formula/checkin.rb
+++ Formula/checkin.rb
@@ -1,22 +1,22 @@
 class Checkin < Formula
   desc "office check-in cli"
   homepage "https://github.com/bjcgg/checkin"
-  url "https://github.com/bjcgg/checkin/releases/download/v0.1.0/checkin-darwin-arm64"
-  version "0.1.0"
+  url "https://github.com/bjcgg/checkin/releases/download/v0.2.0/checkin-darwin-arm64"
+  version "0.2.0"
   license "MIT"
-  sha256 "098413884dcb492ade7db7390daed5ac5af8a58d2a4d0aa8a1ecbe05f51f2faa"
+  sha256 "2a6c497486bd91467514ae7d596b4dbd3f64f87802c2d924402b91bb49b7fd19"
 
   on_macos do
     on_intel do
-      url "https://github.com/bjcgg/checkin/releases/download/v0.1.0/checkin-darwin-x64"
-      sha256 "9d1b0cb649036aef5740857c3194d605424934bbc7597b9805b4dc3a508ab6cf"
+      url "https://github.com/bjcgg/checkin/releases/download/v0.2.0/checkin-darwin-x64"
+      sha256 "8f6d3b0c8111af1be681c186d4d995f33953bd0004bb62eb80e04bdbd4a9e200"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bjcgg/checkin/releases/download/v0.1.0/checkin-linux-x64"
-      sha256 "0a72fd338ba91605cd8ab5c5e43857f1f6e2792491d6a241685f457cc95bdc49"
+      url "https://github.com/bjcgg/checkin/releases/download/v0.2.0/checkin-linux-x64"
+      sha256 "e0fcedd0da55589a0327356eda76ca4e30c877305a9b1473025fd61cf051563f"
     end
   end
 
