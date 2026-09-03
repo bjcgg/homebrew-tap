ece18fea4d962451c7be0705e97330048a6c7b72
Axel Talmet <a@bjc.gg>
2026-09-03 06:31:06 +0100

    office 0.3.3 (packed envelope)

--- Formula/office.rb
+++ Formula/office.rb
@@ -4,19 +4,19 @@ class Office < Formula
   url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.3.3/office-darwin-arm64"
   version "0.3.3"
   license "MIT"
-  sha256 "ac91e0050418c159090c4bc046beea31d76d59b5a75116a420a3b4fe21e68c2e"
+  sha256 "52d96ed03706f5dc8ae5994e752a770dd1ef9c3b77fe4c8de62a506acfe86a09"
 
   on_macos do
     on_intel do
       url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.3.3/office-darwin-x64"
-      sha256 "f975d896c8fe449a050bcc860f94c36f053bd0baf4999c90a234830a9ecbdfff"
+      sha256 "1d0666a6043862730402dd0ecd55c6f39300c082501f46ffb5dfd94d56e46a61"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.3.3/office-linux-x64"
-      sha256 "c543dc2619324e2079cf8a8f341ef51bf6ef6837b0415c113faabb9dd0a1a46e"
+      sha256 "b6c98ff0f21c4e6854622d4b40500a6a3b67d07e278845f1b515a64db9a91e6b"
     end
   end
 
