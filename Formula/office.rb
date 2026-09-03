94dac0aa1f2bfde84d7de27e29f63117785f2f65
Axel Talmet <a@bjc.gg>
2026-09-03 02:21:47 +0100

    office 0.3.0

--- Formula/office.rb
+++ Formula/office.rb
@@ -1,22 +1,22 @@
 class Office < Formula
   desc "office check-in cli"
   homepage "https://github.com/bjcgg/office"
-  url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.2.0/office-darwin-arm64"
-  version "0.2.0"
+  url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.3.0/office-darwin-arm64"
+  version "0.3.0"
   license "MIT"
-  sha256 "1db2805d00683b7976776159bfb763f214230f1ee48f70570aca79f78465d586"
+  sha256 "fb68af10201f39feaad5958be5d2a8bff1825d7a0e93d1b5bcc12194f1df4e13"
 
   on_macos do
     on_intel do
-      url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.2.0/office-darwin-x64"
-      sha256 "5b182810bace92777d1fc235e4cfd7c406dcc64fbbd36a0741e944cbb22671e3"
+      url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.3.0/office-darwin-x64"
+      sha256 "5d2dadb84f3ef4242e12b47160f22f79ccbc1dc358d40f4dd4eb0d35b8d3c7b7"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.2.0/office-linux-x64"
-      sha256 "28f5a8b4d31866c00c6c7bd269675bf9db6d3132622b3a9f44c455dcd78d542d"
+      url "https://github.com/bjcgg/homebrew-tap/releases/download/office-v0.3.0/office-linux-x64"
+      sha256 "ee47d4485f455a0e0304ba8944e299f769c177e1ead6b93323c9c39f8b61ed96"
     end
   end
 
